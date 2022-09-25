d35f3b845409d916fa7c3023e18eb51f92e40fe6
Chris Gitmed <chris@Chriss-MacBook-Pro.local>
2022-09-25 15:00:42 -0700

    Enabled method chaining to create more diverse logs; Updated test script

--- shade.js
+++ shade.js
@@ -1,17 +1,46 @@
-export class Shade {
-  static black = (string) => `\x1b[30m${string}\x1b[0m`;
-  static red = (string) => `\x1b[31xm${string}\x1b[0m`;
-  static green = (string) => `\x1b[32m${string}\x1b[0m`;
-  static yellow = (string) => `\x1b[33m${string}\x1b[0m`;
-  static blue = (string) => `\x1b[34m${string}\x1b[0m`;
-  static magenta = (string) => `\x1b[35m${string}\x1b[0m`;
-  static cyan = (string) => `\x1b[36m${string}\x1b[0m`;
-  static white = (string) => `\x1b[37m${string}\x1b[0m`;
+const ansiCodes = {
+  black: '\x1b[30m',
+  red: '\x1b[31m',
+  green: '\x1b[32m',
+  yellow: '\x1b[33m',
+  blue: '\x1b[34m',
+  magenta: '\x1b[35m',
+  cyan: '\x1b[36m',
+  white: '\x1b[37m',
+  reset: '\x1b[0m',
+  bright: '\x1b[1m',
+  dim: '\x1b[2m',
+  underline: '\x1b[4m',
+  reverse: '\x1b[7m',
+  hidden: '\x1b[8m',
+};
 
-  static bright = (string) => `\x1b[1m${string}\x1b[0m`;
-  static dim = (string) => `\x1b[2m${string}\x1b[0m`;
-  static underscore = (string) => `\x1b[4m${string}\x1b[0m`;
-  static blinking = (string) => `\x1b[5m${string}\x1b[0m`;
-  static reverse = (string) => `\x1b[7m${string}\x1b[0m`;
-  static hidden = (string) => `\x1b[7m${string}\x1b[0m`;
+export class Shade {
+  constructor() {
+    this.string = '';
+  }
+
+  static processString = (string, color) => {
+    if (string) {
+      const final = ansiCodes[color] + (this.string || '') + string + ansiCodes.reset;
+      this.string = '';
+      return final;
+    }
+    this.string = ansiCodes[color] + this.string;
+    return this;
+  }
+
+  static black = (string) => this.processString(string, 'black');
+  static red = (string) => this.processString(string, 'red');
+  static green = (string) => this.processString(string, 'green');
+  static yellow = (string) => this.processString(string, 'yellow');
+  static blue = (string) => this.processString(string, 'blue');
+  static magenta = (string) => this.processString(string, 'magenta');
+  static cyan = (string) => this.processString(string, 'cyan');
+  static white = (string) => this.processString(string, 'white');
+  static underline = (string) => this.processString(string, 'underline');
+  static bright = (string) => this.processString(string, 'bright');
+  static dim = (string) => this.processString(string, 'dim');
+  static reverse = (string) => this.processString(string, 'reverse');
+  static hidden = (string) => this.processString(string, 'hidden');
 }
